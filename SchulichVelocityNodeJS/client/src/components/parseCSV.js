bad8c1a48a801e7438b589e409d8c1176e9da37c
arhamHumayun <44730768+arhamHumayun@users.noreply.github.com>
2019-04-27 16:38:01 -0600

    Added full csv parsing functionality. All data is now visible on one graph given an input of csv.

--- SchulichVelocityNodeJS/client/src/components/parseCSV.js
+++ SchulichVelocityNodeJS/client/src/components/parseCSV.js
@@ -2,24 +2,6 @@ import React, { Component } from 'react';
 import { CSVReader } from 'react-papaparse';
 import { Line } from 'react-chartjs-2';
 import 'chartjs-plugin-streaming';
-import {forEach} from "react-bootstrap/es/utils/ElementChildren";
-
-// 1. choose xData
-// 2. choose yDatas, since we can choose more than one
-// 3. parse and present
-// this allows the user to be able to see very custom charts
-
-let intervals = [];
-let yData = [];
-
-function createPlots(data) {
-    for(let i = 0; i < data.length; i++) {
-        intervals[i] = data[i].Interval; //Interval = time header
-        yData[i] = parseInt(data[i].Speed, 10);
-    }
-
-    return [intervals, yData];
-}
 
 class ParseCSV extends Component {
     constructor(props) {
@@ -40,14 +22,23 @@ class ParseCSV extends Component {
 
         for(let i = 0; i < data.data.length; i++) {
             this.chartData.labels[i] = data.data[i].Interval; //Interval = time header
-            this.chartData.datasets[0].data[i] = parseInt(data.data[i].Speed, 10);
+            //this.chartData.datasets[0].data[i] = parseInt(data.data[i].Speed, 10);
         }
 
-        console.log(data.data);
-
-        //let result = createPlots(data.data);
-        //this.chartData.labels = result[0];
-        //this.chartData.datasets[0].data = result[1];
+        let uneString = Object.keys(data.data[0]);
+        for (let i = 1; i < Object.keys(data.data[0]).length; i++) {
+            this.chartData.datasets.push({
+                data: [],
+                borderColor: 'rgb(255, 0, 0)',
+                backgroundColor: 'rgba(0,0,0,0.0)',
+                lineTension: 0,
+                hidden: true,
+                label: uneString[i],
+            });
+            for(let j = 0; j < data.data.length; j++) {
+                this.chartData.datasets[i].data.push(parseFloat(data.data[j][uneString[i]] , 10));
+            }
+        }
 
         this.forceUpdate();
     };
@@ -70,10 +61,17 @@ class ParseCSV extends Component {
                     style={{display: 'none'}}
                     onError={this.handleOnError}
                     configOptions={{header: true,
-                    delimiter: ',' }}
+                                    delimiter: ','
+                    }}
                 />
                 <button onClick={this.handleImportOffer}>Import</button>
-                <Line data={this.chartData} />
+                <Line data={this.chartData}
+                      options={{
+                          animation: {
+                              duration: 0,
+                          }
+                      }}
+                />
             </div>
         );
     }
