1f285b7129db0d0bebf0d2fd4cd2224da2d9e35d
arhamHumayun <44730768+arhamHumayun@users.noreply.github.com>
2019-06-08 13:26:35 -0600

    Changing graphs now doesnt stack data from last graph

--- SchulichVelocityNodeJS/client/src/components/historical/historicalGraph.js
+++ SchulichVelocityNodeJS/client/src/components/historical/historicalGraph.js
@@ -84,6 +84,10 @@ export default class HistoricalGraph extends Component {
 
         if(data[0] === "Line") {
             chartType = "Line";
+            lineData ={
+                labels: [],
+                datasets: []
+            };
             lineData.labels = data[1].data;
             for(let i = 2; i < data.length; i++) {
                 lineData.datasets.push( {
@@ -101,6 +105,24 @@ export default class HistoricalGraph extends Component {
 
         else if(data[0] === "Scatter") {
             chartType = "Scatter";
+            scatterData = {
+                labels: 'Points',
+                datasets: [{
+                    data: [
+                    ],
+                    fill: false,
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    pointBorderColor: 'rgba(75,192,192,1)',
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                    pointHoverBorderColor: 'rgba(220,220,220,1)',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10
+                }],
+            };
             console.log(data);
             for(let i = 0; i < data[1].data.length; i++) {
                 if (!(isNaN(data[2].data[i]) || isNaN(data[3].data[i]))
