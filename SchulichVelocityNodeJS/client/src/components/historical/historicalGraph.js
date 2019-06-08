ba9f6de536072f89c414022b2131d60399e066ae
arhamHumayun <44730768+arhamHumayun@users.noreply.github.com>
2019-06-08 15:44:03 -0600

    Range Slider is added, still buggy, need to finalize

--- SchulichVelocityNodeJS/client/src/components/historical/historicalGraph.js
+++ SchulichVelocityNodeJS/client/src/components/historical/historicalGraph.js
@@ -1,41 +1,30 @@
 import React, { Component } from 'react';
 import { Line, Scatter } from 'react-chartjs-2';
 import 'chartjs-plugin-streaming';
-import Slider, { Range } from 'rc-slider';
+import Slider,{ Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
 
 let chartType = "Line";
 let chartData = 0;
-let minInterval = 0;
-let maxInterval = 1000;
-let min = 100;
-let max = 500;
-let lineData ={
-    labels: [],
-    datasets: []
-};
-let scatterData = {
-    labels: 'Points',
-    datasets: [{
-        data: [
-        ],
-        fill: false,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10
-    }],
-};
+let minInterval;
+let maxInterval;
+let min;
+let max;
+let originalLineData = {};
+let originalScatterData = {};
 
 export default class HistoricalGraph extends Component {
     constructor(props){
         super(props);
+
+        this.state = {
+            min: 0,
+            max: 100,
+            value: [0, 0],
+            lineData: {},
+            scatterData: {}
+        };
+
         this.options = {
             layout: {
                 padding: {
@@ -82,15 +71,21 @@ export default class HistoricalGraph extends Component {
                           'rgb(255, 255, 0)', 'rgb(0, 255, 255)', 'rgb(255, 0, 255)'];
         chartData = data;
 
+        minInterval = data[1].data[0];
+        maxInterval = data[1].data[data[1].data.length - 2];
+
+        min = minInterval;
+        max = maxInterval;
+
         if(data[0] === "Line") {
             chartType = "Line";
-            lineData ={
+            originalLineData ={
                 labels: [],
                 datasets: []
             };
-            lineData.labels = data[1].data;
+            originalLineData.labels = data[1].data;
             for(let i = 2; i < data.length; i++) {
-                lineData.datasets.push( {
+                originalLineData.datasets.push( {
                     data : data[i].data,
                     label: data[i].label,
                     borderColor: colorArray[i - 2],
@@ -101,11 +96,12 @@ export default class HistoricalGraph extends Component {
                     lineTension: 0
                 })
             }
+            this.setState({lineData: originalLineData});
         }
 
         else if(data[0] === "Scatter") {
             chartType = "Scatter";
-            scatterData = {
+            originalScatterData = {
                 labels: 'Points',
                 datasets: [{
                     data: [
@@ -127,49 +123,97 @@ export default class HistoricalGraph extends Component {
             for(let i = 0; i < data[1].data.length; i++) {
                 if (!(isNaN(data[2].data[i]) || isNaN(data[3].data[i]))
                     && (data[2].data[i] !== 0 || data[3].data[i] !== 0)) {
-                    scatterData.datasets[0].data.push({
+                    originalScatterData.datasets[0].data.push({
                         x: data[2].data[i],
                         y: data[3].data[i]
                     });
                 }
             }
+            this.setState({scatterData: originalScatterData});
         }
         // console.log(lineData);
         // console.log(scatterData);
     };
 
-
-
     rescale(value) {
         console.log('');
-    }
+    };
 
     resize(smaller) {
         if(smaller === true)
             this.options.layout.padding = 240;
         else
             this.options.layout.padding = 80;
-    }
+    };
+
+    rangeIndicatorUpdate = value => {
+        return <p> Current range: {value} </p>
+    };
+
+    rangeUpdate = (value) => {
+
+        //this.rangeIndicatorUpdate(value);
+
+        this.setState({value});
+        console.log(value);
+
+        if (chartType === "Line") {
+
+            this.setState({
+                lineData: originalLineData
+            });
+
+            for(let i = 0; i < originalLineData.labels.length; i++) {
+                if (i < value[0]) {
+                    this.state.lineData.labels[i] = NaN;
+                    for (let j = 0; j < originalLineData.datasets.length; j++) {
+                        this.state.lineData.datasets[j].data[i] = NaN;
+                    }
+                }
+                if (i > value[1]) {
+                    this.state.lineData.labels[i] = NaN;
+                    for (let j = 0; j < originalLineData.datasets.length; j++) {
+                        this.state.lineData.datasets[j].data[i] = NaN;
+                    }
+                }
+            }
+            this.setState({
+                lineData: this.state.lineData
+            });
+
+            this.forceUpdate();
+
+            console.log(originalLineData);
+            console.log(this.state.lineData);
+        }
+
+        else if (chartType === "Scatter") {
+
+        }
+
+    };
+
 
     render() {
         if (chartType === "Line")
             return (
                 <div>
-                    <Line data={lineData} options={this.options}/>
-                    <p>range (0, 1000)</p>
+                    <Line data={this.state.lineData} options={this.options}/>
                     <Range
                         step={20}
                         min={minInterval}
                         max={maxInterval}
                         allowCross={true}
                         defaultValue={[minInterval, maxInterval]}
+                        onAfterChange={this.rangeUpdate}
+                        pushable={1000}
                     />
                 </div>
         );
         else if (chartType === "Scatter")
             return (
                 <div>
-                    <Scatter data={scatterData} options={this.options}/>
+                    <Scatter data={this.state.scatterData} options={this.options}/>
                 </div>
             );
         else
