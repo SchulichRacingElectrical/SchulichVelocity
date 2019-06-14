4c995f5ddae36b0d64dbb8db9ee8202bfe7d4781
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2019-06-13 20:25:37 -0600

    Append to previous commit

--- SchulichVelocityNodeJS/backend/SVserver.js
+++ SchulichVelocityNodeJS/backend/SVserver.js
@@ -76,7 +76,7 @@ class Server {
         this.subscriber.subscribe("streaming");
         this.subscriber.on("message", function (channel, message) {
             this.data = JSON.parse(message);
-            console.log(data);
+            console.log(this.data);
         });
     }
 }
