ed8614340bc21f10cdcfbf66238065a2b6b55260
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2019-06-14 20:42:17 -0600

    Append to previous commit

--- SchulichVelocityNodeJS/client/src/components/streaming/streaming.js
+++ SchulichVelocityNodeJS/client/src/components/streaming/streaming.js
@@ -32,7 +32,14 @@ export default class Streaming extends Component {
     };
 
     getData = async (request) => {
-        this.state.data = await fetch('/api/getStreamingData', {
+        await fetch('/api/request', {
+            method: 'POST', 
+            headers: {
+              'Content-Type': 'application/json',
+            }, 
+            body: JSON.stringify({post: 'streaming'})
+          });
+        fetch('/api/getStreamingData', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
