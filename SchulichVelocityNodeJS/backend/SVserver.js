8b69df5b2e5e0ba7a2a39367f70e61c5f627d566
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2019-06-14 20:23:20 -0600

    Append to previous commit

--- SchulichVelocityNodeJS/backend/SVserver.js
+++ SchulichVelocityNodeJS/backend/SVserver.js
@@ -72,11 +72,10 @@ class Server {
         this.app.post('/api/submitCSV', async (req, res) => {
             //Use CSV controller to call CSV Model which will parse out the csv to properly insert into a table
         });
-        // this.subscriber.subscribe("streaming");
-        // this.subscriber.on("message", function (channel, message) {
-        //     this.data = JSON.parse(message);
-        //     console.log(this.data);
-        // });
+        this.subscriber.subscribe("streaming");
+         this.subscriber.on("message", function (channel, message) {
+             this.data = JSON.parse(message);
+         });
     }
 }
 
